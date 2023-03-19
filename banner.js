a266e944492f65253491a6beeb9a90baf41311f9
FluxOfficial <fluxcontentofficial@gmail.com>
2023-03-19 17:36:19 +0100

    minor update

--- banner.js
+++ banner.js
@@ -19,15 +19,7 @@ document.write(`
     <a href="../browse.html">Browse</a>
   </div>
   <button class="topjumper" onclick="topFunction()" title="Go to top"><i class="arrow up"></i></button>
-<a href="../index.html" class="title">FL<span style="
-    background-image: linear-gradient(5deg, #111010, #ca3025);
-    background-size: 100%;
-    -webkit-background-clip: text;
-  -moz-background-clip: text;
-    -webkit-text-fill-color: transparent; 
-    -moz-text-fill-color: transparent;
-    filter: drop-shadow(0 0mm 1mm rgb(0, 0, 0));"
-    >U</span>X</a>
+  <a href="index.html" class="title">FL<span class="letteru">U</span>X</a>
 <img class="logo" src="../logo 2.0.png">
 <div class="shadow"></div>
 <div class="bottomcontainer">
