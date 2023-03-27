85656394a50bd11068c42ff2a8cae865e5c286af
FluxOfficial <fluxcontentofficial@gmail.com>
2023-03-27 15:06:35 +0200

    minor update
    updated styles from searchlist
    cursor typing effect

--- main.js
+++ main.js
@@ -284,6 +284,7 @@ function quality(element){
       (document.getElementsByClassName("videoplayer")[0]).getElementsByTagName("p")[0].style.marginBottom = "10px";
       (document.getElementsByClassName("videoplayer")[0]).getElementsByTagName("svg")[0].style.display = "none";
       (document.getElementsByClassName("videoplayer")[0]).getElementsByTagName("div")[0].style.display = "block";
+      document.getElementsByTagName("iframe")[0].setAttribute("sandbox", "allow-scripts allow-forms allow-same-origin allow-popups=false");
     }, 1900);
   }
 }
@@ -413,12 +414,14 @@ function searchlist() {
       if (movielist[x].textContent.toLowerCase().includes(sortmovies[i].toLowerCase())) {
       const li = document.createElement("li");
       const a = document.createElement("a");
+      const span = document.createElement("span");
       const img = document.createElement("img");
-      a.innerText = movielist[x].innerText;
+      a.innerHTML = movielist[x].innerText.slice(0,-7) + "<br>";
+      span.innerText = ((movielist[x].innerText).slice(-5)).slice(0,-1);
       li.setAttribute('onclick','openitem(this)');
       img.src = (movielist[x].parentNode).getElementsByClassName("thumbnail")[0].src;
       (ul.appendChild(li)).appendChild(img);
-      (ul.appendChild(li)).appendChild(a);
+      ((ul.appendChild(li)).appendChild(a)).appendChild(span);
       }
     }
   }
@@ -429,7 +432,8 @@ function openitem(element) {
   let content = (iframecontent.getElementsByTagName("html")[0]).getElementsByTagName("body")[0];
   let childs = content.children;
   let numb = content.childElementCount;
-  let itemtitle = element.innerText.toLowerCase();
+  let itemtitle = (element.getElementsByTagName("a")[0].textContent);
+  itemtitle = (itemtitle.slice(0, -4) + " (" + itemtitle.slice(-4) + ")").toLowerCase();
   for (let i = 0; i < numb; i ++) {
     if (itemtitle == childs.item(i).getElementsByClassName("movienames")[0].innerText.toLowerCase()) {
       alldata(childs.item(i));
@@ -557,12 +561,12 @@ function loadmovies() {
   let iframe = document.getElementById('database');
   let iframecontent = iframe.contentDocument || iframe.contentWindow.document;
   let content = (iframecontent.getElementsByTagName("html")[0]).getElementsByTagName("body")[0];
-  // if (content.getElementsByTagName('script')[0]) {
-  //   var ele = content.getElementsByTagName('script')[0];
-  // }
-  // if(ele.parentNode){
-  //   ele.parentNode.removeChild(ele);
-  // }
+  if (content.getElementsByTagName('script')[0]) {
+    var ele = content.getElementsByTagName('script')[0];
+  }
+  if(ele.parentNode){
+    ele.parentNode.removeChild(ele);
+  }
 
   let childs = content.children;
   let numb = content.childElementCount;
@@ -600,12 +604,12 @@ function checkfavorites() {
   let iframe = document.getElementById('database');
   let iframecontent = iframe.contentDocument || iframe.contentWindow.document;
   let content = (iframecontent.getElementsByTagName("html")[0]).getElementsByTagName("body")[0];
-  // if (content.getElementsByTagName('script')[0]) {
-  //   var ele = content.getElementsByTagName('script')[0];
-  // }
-  // if(ele.parentNode){
-  //   ele.parentNode.removeChild(ele);
-  // }
+  if (content.getElementsByTagName('script')[0]) {
+    var ele = content.getElementsByTagName('script')[0];
+  }
+  if(ele.parentNode){
+    ele.parentNode.removeChild(ele);
+  }
   let childs = content.children;
   let numb = content.childElementCount;
   let moviedir = document.getElementsByClassName("recent")[0];
